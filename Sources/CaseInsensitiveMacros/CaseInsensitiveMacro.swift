8f427e9e0b8eb03a49bc80c002902c55bc0ee643
Kota1021 <ko_matsumoto@yumemi.co.jp>
2023-10-28 11:36:07 +0900

    removed conformance to CaseIterable,

--- Sources/CaseInsensitiveMacros/CaseInsensitiveMacro.swift
+++ Sources/CaseInsensitiveMacros/CaseInsensitiveMacro.swift
@@ -6,7 +6,6 @@ import SwiftDiagnostics
 
 /// Implementation of the `CaseInsensitive` macro.
 public struct CaseInsensitiveMacro: MemberMacro {
-    
     /// Unique identifier for messages related to this macro.
     private static let messageID = MessageID(domain: "CaseInsensitiveMacro", id: "EnumDefaultImplementation")
     
@@ -17,88 +16,56 @@ public struct CaseInsensitiveMacro: MemberMacro {
         in context: some MacroExpansionContext
     ) throws -> [DeclSyntax] {
         // Validate that the macro is being applied to a enum declaration
-        guard declaration.is(EnumDeclSyntax.self) else {
+        guard let enumDecl = declaration.as(EnumDeclSyntax.self) else {
             throw SimpleDiagnosticMessage(
-              message: "Macro `CaseInsensitive` can only be applied to a enum",
-              diagnosticID: messageID,
-              severity: .error
+                message: "Macro `CaseInsensitive` can only be applied to a enum",
+                diagnosticID: messageID,
+                severity: .error
             )
         }
         
-        
-        // TODO: Now DeclSyntax is written in string literal, however, should be written with Syntax Node Types.
-            let initializer: DeclSyntax =
-              """
-              init?(rawValue: String) {
-                  let area = Area.allCases.first { rawValue.lowercased() == $0.rawValue.lowercased() }
-                  guard let area else { return nil }
-                  self = area
-              }
-              """
-            return [initializer]
+        // Validate that the enum has RawValue of `String` type
+        guard checkIfHasStringRawValue(enumDecl) else {
+            throw SimpleDiagnosticMessage(
+                message: "Macro `CaseInsensitive` can only be applied to a enum with raw value of `String` type",
+                diagnosticID: messageID,
+                severity: .error
+            )
+        }
+        let initializer = try generateInitForStringRawValue(enumDecl: enumDecl)
+        return [initializer.as(DeclSyntax.self)!]
     }
 }
 
-extension CaseInsensitiveMacro: ExtensionMacro {
-    
-    // MARK: Conforms to ExtensionMacro to conform to CaseIterable
-    public static func expansion(
-         of node: SwiftSyntax.AttributeSyntax,
-         attachedTo declaration: some SwiftSyntax.DeclGroupSyntax,
-         providingExtensionsOf type: some SwiftSyntax.TypeSyntaxProtocol,
-         conformingTo protocols: [SwiftSyntax.TypeSyntax],
-         in context: some SwiftSyntaxMacros.MacroExpansionContext
-    ) throws -> [SwiftSyntax.ExtensionDeclSyntax] {
-        guard let enumDecl = declaration.as(EnumDeclSyntax.self) else {
-          throw SimpleDiagnosticMessage(
-            message: "Macro `CaseInsensitive` can only be applied to a enum",
-            diagnosticID: messageID,
-            severity: .error
-          )
-        }
-        
-        // Check if Enum on which this macro is attached to conforms to RawRepresentable.
-        guard checkIfEnumHasRawValue(enumDecl: enumDecl) else { throw throwNoRawValue() }
-        
-        //https://forums.swift.org/t/examples-of-extensionmacro/66717/9
-        let caseIterableExtension: DeclSyntax =
-              """
-              extension \(type.trimmed): CaseIterable {}
-              """
-
-        guard let extensionDecl = caseIterableExtension.as(ExtensionDeclSyntax.self) else {
-          return []
-        }
-
-        return [extensionDecl]
-    }
-    
-    public static func checkIfEnumHasRawValue(enumDecl: EnumDeclSyntax) -> Bool {
-        guard let inheritanceClause = enumDecl.inheritanceClause else {
-            return false
-        }
+extension CaseInsensitiveMacro {
+    private static func checkIfHasStringRawValue(_ enumDecl: EnumDeclSyntax) -> Bool {
+        guard let inheritanceClause = enumDecl.inheritanceClause else { return false }
         let inheritedTypes: InheritedTypeListSyntax = inheritanceClause.inheritedTypes
-        let inheritedTypeNames: [String] = inheritedTypes.map { inheritedType in
-            guard let identifierType = inheritedType.type.as(IdentifierTypeSyntax.self) else {
-                preconditionFailure()
-            }
-            return identifierType.name.text
-        }
-        let hasRawValue: Bool =
-            inheritedTypeNames.contains("String") ||
-            inheritedTypeNames.contains("Character") ||
-            inheritedTypeNames.contains("Int") ||
-            inheritedTypeNames.contains("Float")
-        
-        return hasRawValue
+        guard let firstInheritedType = inheritedTypes.first else { return false }
+        guard let type: TypeSyntax = firstInheritedType.type.as(TypeSyntax.self) else { return false }
+        guard "String" == type.trimmed.description else { return false } // RawValue on Enum may also be Int or Float.
+        return true
     }
     
-    public static func throwNoRawValue() -> Error {
-         SimpleDiagnosticMessage(
-          message: "Macro `CaseInsensitive` can only be applied to a enum with RawValue",
-          diagnosticID: messageID,
-          severity: .error
-        )
+    private static func generateInitForStringRawValue(enumDecl: EnumDeclSyntax) throws -> InitializerDeclSyntax {
+        let members = enumDecl.memberBlock.members
+        let caseDecl = members.compactMap { $0.decl.as(EnumCaseDeclSyntax.self) }
+        let elements = caseDecl.flatMap { $0.elements }
+        
+        let initializer =  try InitializerDeclSyntax("init?(rawValue: String)") {
+            try SwitchExprSyntax("switch rawValue.lowercased()") {
+                for element in elements {
+                    SwitchCaseSyntax(
+                        """
+                        case \"\(raw: "\(element.name)".lowercased())\":
+                            self = .\(element.name)
+                        """
+                    )
+                }
+                SwitchCaseSyntax("default: return nil")
+            }
+        }
+        return initializer
     }
 }
 
@@ -113,19 +80,4 @@ struct SimpleDiagnosticMessage: DiagnosticMessage, Error {
   let message: String
   let diagnosticID: MessageID
   let severity: DiagnosticSeverity
-}
-
-extension SimpleDiagnosticMessage: FixItMessage {
-  var fixItID: MessageID { diagnosticID }
-}
-
-enum CustomError: Error, CustomStringConvertible {
-  case message(String)
-
-  var description: String {
-    switch self {
-    case .message(let text):
-      return text
-    }
-  }
 }
